c02124f984a286f9b6a0c0aa6a0119ed5a4b146e
Taylor Hughes <taylor hughes>
2022-02-20 21:19:40 -0800

    added API retrieval

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
 import Nav from './components/nav'
+const contentful = require('contentful')
 
 function App() {
+
+  /////////////////////////////
+  // Constants
+  /////////////////////////////
+
+  let client = contentful.createClient({
+    space: process.env.REACT_APP_SPACE_KEY,
+    accessToken: process.env.REACT_APP_API_TOKEN,
+  });
+  const [entries, setEntries] = useState([])
+
+
+  /////////////////////////////
+  // Functions
+  /////////////////////////////
+
+  
+  
+  /////////////////////////////
+  // Render
+  /////////////////////////////
+  useEffect(() => {
+    client.getEntries().then(function (entries) {
+      // log the title for all the entries that have it
+      setEntries(entries.items)
+    });
+  }, [])
+
+  console.log(entries);
+
   return (
     <div className="App">
       <h1>Ten Dollar Shakespeare</h1>
