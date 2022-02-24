a0e7156d5918159bbfabef80f7db29386f237c49
Taylor Hughes <taylor hughes>
2022-02-23 18:11:20 -0800

    completed About section

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -18,7 +18,30 @@ const About = (props) => {
     /////////////////////////////
 
     return (
-        <h2>This is the About Page</h2>
+        <div id='about'>
+            <h3>Who is $10 Shakespeare?</h3>
+            <p>$10 Shakespeare is a proletariat based out of Los Angeles, CA whose mission it is to sniff out as much cheap local Shakespeare in California as is possible.</p>
+            <p>Part of this includes somewhat of a vagabond lifestyle - you will hear mention of sleeping in cars, working remotely from coffee shops, frequenting local diners and dive bars, and the sneaking of libations into theaters. I suspect that as we enter further into the heart of the 21st century, these practices will only intensify on my part.</p>
+
+            <h3>Would you like your local production displayed on my site?</h3>
+            <p>Feel free to contact me at my personal website, <a href='https://taylorzhughes.com' target='_blank'>taylorzhughes.com</a>. I would be happy to display your production here on my website.</p>
+
+            <h3>Why are some productions over $10?</h3>
+            <p>There could be many reasons why I don't always stick to the hard and fast number, chief of all being that almost nothing costs $10 anymore.</p>
+            <p>Also, much like how periodically the cheap wine-drinker may splurge on a special occasion, I may from time to time splurge on a higher quality production. If I ever end up in London, for instance, I will pay as much as is required to attend a production at the Globe theater. I would consider that my Super Bowl.</p>
+            <p>For the most part though, the productions listed will fit in the $10 - $20 range.</p>
+
+            <h3>Why we do this</h3>
+            <p>We appreciate the spirit of those who put in countless hours to provide quality entertainment, not for the money, but for the love of the discipline.</p>
+            <p>We appreciate that we are part of the filthy masses in which these productions were originally intended centuries ago.</p>
+            <p>We appreciate that all entertainment distills down to a similar spirit of passion and greatness, be it at London’s Globe or at your local high school auditorium.</p>
+            <p>We appreciate the almighty dollar.</p>
+            <p>We appreciate the level of mastery required to write dick jokes that remain funny 400 years after they were penned.</p>
+            <p>We appreciate the heroes, the villains, and most of all the fools.</p>
+            <p>We appreciate those who always remember their lines, and we especially appreciate those who don’t.</p>
+            <p>Most of all, we appreciate you, the reader. Thank you!</p>
+
+        </div>
     )
 }
 
